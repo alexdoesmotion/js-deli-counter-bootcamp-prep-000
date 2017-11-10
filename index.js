d25eeb080ed250166a35322fc2765d1676baaae3
Alex Johnson <alex@armadillomotion.com>
2017-11-10 22:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(name)
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(katzDeli) {
+  if (katzDeli.length > 0) {
+    
+  }
+  else {
+    return `There is nobody waiting to be served!`
+  }
+}
